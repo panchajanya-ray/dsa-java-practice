5e9bd5be1ed3d483de9a59b2dcb5f36e2b0d2587
panchajanya <panchajanya.r@gmail.com>
2026-03-06 22:07:45 +0530

    Added new package and solved one problem in sliding window pattern

---
+++ src/slidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,27 @@
+// LeetCode 3. Longest Substring Without Repeating Characters
+// Pattern: Two Pointer
+// Time Complexity: O(n)
+// Space Complexity: O(min(n , m))
+
+package slidingWindow;
+
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+	public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int length = 0;
+        for(int right=0;right<s.length();right++)
+        {
+            while(set.contains(s.charAt(right)))
+            {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            length = Math.max(length,right-left+1);
+        }
+        return length;
+    }
+}
