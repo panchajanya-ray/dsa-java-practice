617296b9b8f7b9d8f15af9b104a34d3bea7dad70
panchajanya <panchajanya.r@gmail.com>
2026-03-09 22:33:10 +0530

    Day 8: Solve Search Insert Position (Easy) - Binary Search

---
+++ src/binarySearch/SearchInsertPosition.java
@@ -0,0 +1,21 @@
+// LeetCode 35. Search Insert Position
+// Pattern: Binary Search
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+package binarySearch;
+
+public class SearchInsertPosition {
+	public int searchInsert(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right)
+        {
+            int mid = (left+right)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) right=mid-1;
+            else left=mid+1;
+        }
+        return left;
+    }
+}
