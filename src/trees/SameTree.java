a49d149e866c8a25a87cf024fd050d9fc598a0ec
panchajanya <panchajanya.r@gmail.com>
2026-03-11 19:56:54 +0530

    Day 10: Solve Same Tree (Easy) - Binary Tree Recursion

---
+++ src/trees/SameTree.java
@@ -0,0 +1,33 @@
+// LeetCode 100. Same Tree
+// Pattern: Binary Tree
+// Time Complexity: O(n)
+// Space Complexity: O(h), h = height of tree
+// Space Complexity: O(n) (Worst Case)
+
+package trees;
+
+//Definition for a binary tree node.
+/*
+class TreeNode {
+int val;
+TreeNode left;
+TreeNode right;
+TreeNode() {}
+TreeNode(int val) { this.val = val; }
+TreeNode(int val, TreeNode left, TreeNode right) {
+   this.val = val;
+   this.left = left;
+   this.right = right;
+}
+}
+*/
+
+public class SameTree {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p== null && q == null) return true;
+
+        if(p==null || q==null || p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
