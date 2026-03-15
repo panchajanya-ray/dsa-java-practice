8046f799105802abbda77379bd484eba3a3b2f68
panchajanya <panchajanya.r@gmail.com>
2026-03-15 23:47:31 +0530

    Day 14: Solve Partition Equal Subset Sum Problem (Medium) - Dynamic Programming, 0/1 Knapsack, Subset Sum

---
+++ src/dp/PartitionEqualSubsetSum.java
@@ -0,0 +1,44 @@
+// LeetCode 416. Partition Equal Subset Sum
+// Pattern: Dynamic Programming, 0/1 Knapsack, Sum of Subset Problem
+// Time Complexity: O(n * S), S = Sum/2
+// Space Complexity: O(n * S), S = Sum/2
+
+package dp;
+
+public class PartitionEqualSubsetSum {
+	public boolean canPartition(int[] nums) {
+        int n= nums.length;
+        int sum=0;
+        for(int num: nums) sum+= num;
+        
+        if(sum%2 != 0) return false;
+
+        boolean t[][] = new boolean[n+1][sum/2+1];
+
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<(sum/2+1);j++)
+            {
+                if(i==0) t[i][j]=false;
+                if(j==0) t[i][j]=true;
+            }
+        }
+
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<(sum/2+1);j++)
+            {
+                if(nums[i-1]<=j)
+                {
+                    t[i][j] = t[i-1][j] || t[i-1][j-nums[i-1]];
+                }
+                else
+                {
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+
+        return t[n][sum/2];
+    }
+}
