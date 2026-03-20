857a982a66e2b8dd6604d6b98bd3da75569da82c
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-03-20 22:55:53 +0530

    Day 19: Solve Binary Tree Preorder Traversal (Easy) - DFS

---
+++ src/trees/BinaryTreePreorderTraversal.java
@@ -0,0 +1,20 @@
+// LeetCode 144. Binary Tree Preorder Traversal
+// Pattern: Dynamic Programming
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+	List<Integer> order = new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) return order;
+        order.add(root.val);
+        if(root.left != null) preorderTraversal(root.left);
+        if(root.right != null) preorderTraversal(root.right);
+        return order;
+    }
+}
