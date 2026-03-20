0a134c442f6b5d287d1e7631b44eaad2a77489c9
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-03-20 21:21:15 +0530

    Day 19: Solve Fibonacci Number (Easy) - DP Basics

---
+++ src/dp/FibonacciNumber.java
@@ -0,0 +1,20 @@
+// LeetCode 509. Fibonacci Number
+// Pattern: Dynamic Programming
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+package dp;
+
+public class FibonacciNumber {
+	public int fib(int n) {
+        if(n==0 || n==1) return n;
+        int[] fib = new int[n+1];
+        fib[0]=0;
+        fib[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            fib[i] = fib[i-1]+fib[i-2];
+        }
+        return fib[n];
+    }
+}
