1000f3dc4867ae117f6d73884688274a708313f2
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-03-23 15:51:02 +0530

    Day 22: Solved Reversed Integer (Medium) - Number maniplation, no. less than 32 bit integer

---
+++ src/string/ReverseInteger.java
@@ -0,0 +1,29 @@
+// LeetCode 7. Reverse Integer
+// Pattern: Number manipulation
+// Time Complexity: O(d), d= number of digits
+// Space Complexity: O(1)
+
+package string;
+
+public class ReverseInteger {
+	public int reverse(int x) 
+    {
+        long rev = 0;
+        long abs = Math.abs((long) x);
+
+        while (abs > 0) 
+        {
+            rev = (rev * 10) + (abs % 10);
+            abs = abs / 10;
+        }
+
+        if (x < 0) rev *= -1;
+
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) 
+        {
+            return 0;
+        }
+
+        return (int) rev;
+    }
+}
