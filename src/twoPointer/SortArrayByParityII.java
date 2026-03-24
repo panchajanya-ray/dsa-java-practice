808b0231724bd42f3e811d9856840381c1593625
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-03-24 18:24:18 +0530

    Day 23: Solved Sorting array by even first and then odd elt (Easy) - two Pointers

---
+++ src/twoPointer/SortArrayByParityII.java
@@ -0,0 +1,30 @@
+// LeetCode 922. Sort Array By Parity II
+// Pattern: Two Pointers
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+package twoPointer;
+
+public class SortArrayByParityII {
+	public int[] sortArrayByParityII(int[] nums) {
+        int n = nums.length;
+        if(n==2 && nums[0]%2==0) return nums;
+        int i = 0;
+        int j = 1;
+        int[] ans = new int[n];
+        for(int a = 0;a<n;a++)
+        {
+            if(nums[a]%2==0)
+            {
+                ans[i] = nums[a];
+                i+=2;
+            }
+            else
+            {
+                ans[j] = nums[a];
+                j+=2;
+            }
+        }
+        return ans;
+    }
+}
