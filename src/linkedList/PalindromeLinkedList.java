33e4a9357151e5b9280da07e2cc91246585c3e4d
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-03-29 22:03:11 +0530

    updated ListNode class

--- src/linkedList/PalindromeLinkedList.java
+++ src/linkedList/PalindromeLinkedList.java
@@ -5,6 +5,17 @@
 
 package linkedList;
 
+//Definition for singly-linked list.
+/*
+class ListNode {
+	 int val;
+ListNode next;
+ListNode() {}
+ListNode(int val) { this.val = val; }
+ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+*/
+
 public class PalindromeLinkedList {
 	public boolean isPalindrome(ListNode head) {
         ListNode fast = head;
