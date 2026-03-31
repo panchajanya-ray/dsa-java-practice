f6170732bf513878f1ccb4b97740b3c6e2ff0fe7
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-03-31 23:13:52 +0530

    Day 30: Solved reverse linked list II (Medium) - reverse between left and right pointer

---
+++ src/linkedList/ReverseLinkedListII.java
@@ -0,0 +1,37 @@
+// LeetCode 92. Reverse Linked List II
+// Pattern: Singly Linked List
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+package linkedList;
+
+//Definition for singly-linked list.
+/*
+class ListNode {
+	 int val;
+ ListNode next;
+ ListNode() {}
+ ListNode(int val) { this.val = val; }
+ ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+*/
+
+public class ReverseLinkedListII {
+	public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        
+        for(int i = 0; i < left - 1; i++) prev = prev.next;
+            
+        
+        ListNode curr = prev.next;
+        for(int i = 0; i < right - left; i++){
+            ListNode forw = curr.next;
+            curr.next = forw.next;
+            forw.next = prev.next;
+            prev.next = forw;
+        }
+        return dummy.next;
+    }
+}
