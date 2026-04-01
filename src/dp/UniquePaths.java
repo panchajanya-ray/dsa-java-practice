996e812f4628bdf5821fffc239d0e59440a58f61
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-04-01 21:30:08 +0530

    Day 30: solved unique path to reach the end of the grid (Medium) - Dynamic Programming

---
+++ src/dp/UniquePaths.java
@@ -0,0 +1,26 @@
+// LeetCode 62. Unique Paths
+// Pattern: Dynamic Programming
+// Time Complexity: O(m * n)
+// Space Complexity: O(n)
+
+package dp;
+
+import java.util.Arrays;
+
+public class UniquePaths {
+	public int uniquePaths(int m, int n) {
+        int[] aboveRow = new int[n];
+        Arrays.fill(aboveRow, 1);
+
+        for (int row = 1; row < m; row++) {
+            int[] currentRow = new int[n];
+            Arrays.fill(currentRow, 1);
+            for (int col = 1; col < n; col++) {
+                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+            }
+            aboveRow = currentRow;
+        }
+
+        return aboveRow[n - 1];        
+    }
+}
