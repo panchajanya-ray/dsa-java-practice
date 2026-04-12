5953c393dd338e0cc5187dcdc88fcfa52a420765
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-04-12 21:55:15 +0530

    Day 42: solved is subsequence problem (Easy) - string

---
+++ src/string/IsSubsequence.java
@@ -0,0 +1,23 @@
+// LeetCode Problem: 392. Is Subsequence
+// Pattern: String
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+package string;
+
+public class IsSubsequence {
+	public boolean isSubsequence(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int i=0;
+        int j=0;
+
+        while(i<m && j<n)
+        {
+            if(s.charAt(i)==t.charAt(j)) i++;
+            j++;
+        }
+
+        return i==m;
+    }
+}
