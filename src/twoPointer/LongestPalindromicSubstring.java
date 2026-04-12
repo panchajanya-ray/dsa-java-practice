d737771c363063cc89472d44cbb375610d797c1f
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-04-12 23:11:05 +0530

    Day 42: solved longest palindromic substring problem (Medium) - Two pointers, dp

---
+++ src/twoPointer/LongestPalindromicSubstring.java
@@ -0,0 +1,36 @@
+// LeetCode Problem: 5. Longest Palindromic Substring
+// Pattern: two pointers, dp
+// Time Complexity: O(N^2)
+// Space Complexity: O(1)
+
+package twoPointer;
+
+public class LongestPalindromicSubstring {
+	public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i); // for odd length palindrome
+            
+            int len2 = expand(s, i, i + 1); // for odd length palindrome
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+	
+	private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
