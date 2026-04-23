2d7d7c1703bb104e78b8a1d26b23b65db54896ef
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-04-23 21:57:36 +0530

    Day 23: solved remove all occurrences of a string problem (Medium) - String Manipulation

---
+++ src/string/RemoveAllOccurrencesOfASubstring.java
@@ -0,0 +1,19 @@
+// LeetCode 1910. Remove All Occurrences of a Substring
+// Pattern: string
+// Time Complexity: O(N^2 / M) or O(N^2), N = length of s & M = length of part
+// Space Complexity: O(N)
+
+package string;
+
+public class RemoveAllOccurrencesOfASubstring {
+	public String removeOccurrences(String s, String part) {
+        int n = s.length();
+        int index = s.indexOf(part);
+        while(index != -1) {
+            s = s.substring(0, index) + s.substring(index + part.length());
+            n = s.length();
+            index = s.indexOf(part);
+        }
+        return s;
+    }
+}
