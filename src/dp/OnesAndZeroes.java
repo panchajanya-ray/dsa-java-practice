adad3683a7da09991cfd57980c7937bbbc8905f7
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-04-28 21:56:12 +0530

    Day 58: solved Ones and zeroes problem (Medium) - Dynamic Programming, 0/ 1 Knapsach problem pattern

---
+++ src/dp/OnesAndZeroes.java
@@ -0,0 +1,22 @@
+// LeetCode 474. Ones and Zeroes
+// Pattern: Dynamic Programming, O/ 1 Knapsack pattern problem
+// Time Complexity: O(L * S + L * m * n), L = length of strs, S = length of each string
+// Space Complexity: O(m * n)
+
+package dp;
+
+public class OnesAndZeroes {
+	public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        for (String str : strs) {
+            int zeros = 0, ones = 0;
+            for (char c : str.toCharArray())
+                if (c == '0') zeros++;
+                else ones++;
+            for (int i = m; i >= zeros; i--)
+                for (int j = n; j >= ones; j--)
+                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
+        }
+        return dp[m][n];
+    }
+}
