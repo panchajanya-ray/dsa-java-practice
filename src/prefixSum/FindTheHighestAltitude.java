ca3c882892dcafb3d4b099ad9d2468ff831ea3d5
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-05-08 23:50:06 +0530

    Day 68: solved find highest altitude problem (Easy) - prefixsum

---
+++ src/prefixSum/FindTheHighestAltitude.java
@@ -0,0 +1,20 @@
+// LeetCode 1732. Find the Highest Altitude
+// Pattern: Prefix Sum, HashMap, Modulo trick
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+package prefixSum;
+
+public class FindTheHighestAltitude {
+	public int largestAltitude(int[] gain) {
+        int max = 0;
+        int n = gain.length;
+        int prefix[] = new int[n+1];
+        for(int i = 1; i < n+1; i++)
+        {
+            prefix[i] = prefix[i-1] + gain[i-1];
+            max = Math.max(max, prefix[i]);
+        }
+        return max;
+    }
+}
