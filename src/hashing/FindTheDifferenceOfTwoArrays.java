674d76f9d3a15e2f66e87f0d24b16348c97210c3
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-05-10 22:01:51 +0530

    Day 70: solved find the difference of two arrays (Easy) - HashSet

---
+++ src/hashing/FindTheDifferenceOfTwoArrays.java
@@ -0,0 +1,31 @@
+// LeetCode 2215. Find the Difference of Two Arrays
+// Pattern: HashSet
+// Time Complexity: O(N + M)
+// Space Complexity: O(N + M)
+
+package hashing;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class FindTheDifferenceOfTwoArrays {
+	public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        List<List<Integer>> ans = new ArrayList<>();
+        HashSet<Integer> set1 = new HashSet<>();
+        for(int num : nums1) set1.add(num);
+
+        HashSet<Integer> set2 = new HashSet<>();
+        for(int num : nums2) set2.add(num);
+
+        List<Integer> list1 = new ArrayList<>();
+        for(int num : set1) if(!set2.contains(num)) list1.add(num);
+        
+        List<Integer> list2 = new ArrayList<>();
+        for(int num : set2) if(!set1.contains(num)) list2.add(num);
+        
+        ans.add(list1);
+        ans.add(list2);
+        return ans;
+    }
+}
