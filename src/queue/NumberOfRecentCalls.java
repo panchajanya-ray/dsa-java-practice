0a359634a09e084c1aab7899b8fc5f526a39fc8e
panchajanya-ray <ray.panchajanya.mca@gmail.com>
2026-05-13 20:48:45 +0530

    Day 72: solved number of recent calls problem (easy) - Queue

---
+++ src/queue/NumberOfRecentCalls.java
@@ -0,0 +1,34 @@
+// LeetCode 933. Number of Recent Calls
+// Pattern: Queue
+// Time Complexity: O(1)
+// Space Complexity: O(W), W = no. of ping call
+
+package queue;
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
+
+public class NumberOfRecentCalls {
+
+}
+
+class RecentCounter {
+    private static final int[] records = new int[10000];
+    private int start;
+    private int end;
+    public RecentCounter() {
+        start = 0;
+        end = 0;
+    }
+    
+    public int ping(int t) {
+        while(start < end && (t - records[start] > 3000)) start++;
+
+        records[end++] = t;
+        return end - start;
+    }
+}
+
